3fa0c937aa7d9fd644d7d97909528748f208083a
floret <sorrowbedamned@gmail.com>
2013-09-01 18:36:57 +0200

    Infinate loop, stack overflow?

--- MineSweeper/CSurroundCount.cs
+++ MineSweeper/CSurroundCount.cs
@@ -9,16 +9,15 @@ namespace MineSweeper
 {
     class CSurroundCount
     {
-        public CSurroundCount()
-        {
+        //public CSurroundCount()
+        //{
 
-        }
-
-        frmMain Main;
-        public CSurroundCount(Form frmCalled)
-        {
-            Main = (frmMain)frmCalled;
-        }
+        //}
+        //Form Main;
+        //public CSurroundCount(Form frmCalled)
+        //{
+        //    Main = (frmMain)frmCalled;
+        //}
         /*
             +----+----+----+                +----+----+----+ 
             |x-1 | x  |x+1 | y-1            | M16| M15|M14 |
@@ -47,16 +46,94 @@ namespace MineSweeper
                         Count = Numbers.MineCount(mybtn, btn_grid);//gets count of mines around mybutton  
                         if (Count == 0)//call expansion from here.
                         {
-                            Main.Expansion(mybtn);//hele fokken form is null.
-
-                            //MessageBox.Show("1");                            
-                            //frmMain.Expansion(mybtn);
-                            //Expansion(mybtn);
+                            Expansion(mybtn, btn_grid);//hele fokken form is null.
                         }
                     }
                 }
             }
             return Count;
+        }
+
+
+        /// <summary>
+        /// expands stuff.
+        /// </summary>
+        public void Expansion(Button myButton, Button[,] btn_grid)//think of better name
+        {
+            for (int x = 0; x < 15; x++)//for the horizontal buttons.
+            {
+                for (int y = 0; y < 15; y++)//for the vertical buttons.
+                {
+                    if (btn_grid[x, y] == myButton)//gets position of mybtn.
+                    {
+                        /*
+                          +----+----+----+
+                          |x-1 | x  |x+1 | 
+                          +----+----+----+ 
+                          |x-1 |mybn|x+1 |
+                          +----+----+----+  
+                          |x-1 | x  |x+1 | 
+                          +----+----+----+ 
+                        */
+                        try
+                        {
+                        Button btn_m1_m1 = btn_grid[x - 1, y - 1];
+                        int i_m1_m1 = When0(btn_m1_m1, btn_grid);
+                        Numbers.DisplayCount(Numbers.MineCount(btn_m1_m1, btn_grid), btn_m1_m1);
+                        }
+                        catch (Exception) { }
+                        try
+                        {
+                            Button btn_0_m1 = btn_grid[x, y - 1];
+                            int i_0_m1 = When0(btn_0_m1, btn_grid);
+                            Numbers.DisplayCount(Numbers.MineCount(btn_0_m1, btn_grid), btn_0_m1);
+                        }
+                        catch (Exception) { }
+                        try
+                        {
+                            Button btn_1_m1 = btn_grid[x + 1, y - 1];
+                            int i_1_m1 = When0(btn_1_m1, btn_grid);
+                            Numbers.DisplayCount(Numbers.MineCount(btn_1_m1, btn_grid), btn_1_m1);
+                        }
+                        catch (Exception) { }
+                        try
+                        {
+                            Button btn_m1_0 = btn_grid[x - 1, y];
+                            int i_m1_0 = When0(btn_m1_0, btn_grid);
+                            Numbers.DisplayCount(Numbers.MineCount(btn_m1_0, btn_grid), btn_m1_0);
+                        }
+                        catch (Exception) { }
+                        try
+                        {
+                            Button btn_1_0 = btn_grid[x + 1, y];
+                            int i_1_0 = When0(btn_1_0, btn_grid);
+                            Numbers.DisplayCount(Numbers.MineCount(btn_1_0, btn_grid), btn_1_0);
+                        }
+                        catch (Exception) { }
+                        try
+                        {
+                            Button btn_m1_1 = btn_grid[x - 1, y + 1];
+                            int i_m1_1 = When0(btn_m1_1, btn_grid);
+                            Numbers.DisplayCount(Numbers.MineCount(btn_m1_1, btn_grid), btn_m1_1);
+                        }
+                        catch (Exception) { }
+                        try
+                        {
+                            Button btn_0_1 = btn_grid[x, y + 1];
+                            int i_0_1 = When0(btn_0_1, btn_grid);
+                            Numbers.DisplayCount(Numbers.MineCount(btn_0_1, btn_grid), btn_0_1);
+                        }
+                        catch (Exception) { }
+                        try
+                        {
+                            Button btn_1_1 = btn_grid[x + 1, y + 1];
+                            int i_1_1 = When0(btn_1_1, btn_grid);
+                            Numbers.DisplayCount(Numbers.MineCount(btn_1_1, btn_grid), btn_1_1);
+                        }
+                        catch (Exception) { }
+                    }
+                }
+            }
         }
 
     }
--- MineSweeper/Form1.cs
+++ MineSweeper/Form1.cs
@@ -15,7 +15,7 @@ namespace MineSweeper
         public frmMain()
         {
             InitializeComponent();
-            CSurroundCount SurroundCount = new CSurroundCount(this);
+            //CSurroundCount SurroundCount = new CSurroundCount(this);
         }
 
         //Custom exeptions.
@@ -25,16 +25,16 @@ namespace MineSweeper
 
 
         //properties        
-        private Button ExpandBtn;
-        public Button setExpandBtn
-        {
-            get { return this.ExpandBtn; }
-            set
-            {
-                Expansion(value);
-                value = ExpandBtn;
-            }
-        }
+        //private Button ExpandBtn;
+        //public Button setExpandBtn
+        //{
+        //    get { return this.ExpandBtn; }
+        //    set
+        //    {
+        //        Expansion(value);
+        //        value = ExpandBtn;
+        //    }
+        //}
 
         private int[,] grid;
         public Button[,] btn_grid;//array of buttons.
@@ -146,7 +146,8 @@ namespace MineSweeper
                         if (mineCountInner == 0)//IF myButton has no mines surrounding it
                         {
                             //make this a method.
-                            Expansion(myButton);
+                            SurroundCount.Expansion(myButton, btn_grid);
+                            //Expansion(myButton);
                         }
                         mineCountInner = 0;//makes CNumbers reusable.
                     }
@@ -164,85 +165,85 @@ namespace MineSweeper
         }
 
 
-        /// <summary>
-        /// expands stuff.
-        /// </summary>
-        public void Expansion(Button myButton)//think of better name
-        {
-            for (int x = 0; x < 15; x++)//for the horizontal buttons.
-            {
-                for (int y = 0; y < 15; y++)//for the vertical buttons.
-                {
-                    if (btn_grid[x, y] == myButton)//gets position of mybtn.
-                    {
-                        /*
-                          +----+----+----+
-                          |x-1 | x  |x+1 | 
-                          +----+----+----+ 
-                          |x-1 |mybn|x+1 |
-                          +----+----+----+  
-                          |x-1 | x  |x+1 | 
-                          +----+----+----+ 
-                        */
-                        //try
-                        //{
-                            Button btn_m1_m1 = btn_grid[x - 1, y - 1];
-                            int i_m1_m1 = SurroundCount.When0(btn_m1_m1, btn_grid);
-                            Numbers.DisplayCount(Numbers.MineCount(btn_m1_m1, btn_grid), btn_m1_m1);
-                        //}
-                        //catch (Exception) { }
-                        try
-                        {
-                            Button btn_0_m1 = btn_grid[x, y - 1];
-                            int i_0_m1 = SurroundCount.When0(btn_0_m1, btn_grid);
-                            Numbers.DisplayCount(Numbers.MineCount(btn_0_m1, btn_grid), btn_0_m1);
-                        }
-                        catch (Exception) { }
-                        try
-                        {
-                            Button btn_1_m1 = btn_grid[x + 1, y - 1];
-                            int i_1_m1 = SurroundCount.When0(btn_1_m1, btn_grid);
-                            Numbers.DisplayCount(Numbers.MineCount(btn_1_m1, btn_grid), btn_1_m1);
-                        }
-                        catch (Exception) { }
-                        try
-                        {
-                            Button btn_m1_0 = btn_grid[x - 1, y];
-                            int i_m1_0 = SurroundCount.When0(btn_m1_0, btn_grid);
-                            Numbers.DisplayCount(Numbers.MineCount(btn_m1_0, btn_grid), btn_m1_0);
-                        }
-                        catch (Exception) { }
-                        try
-                        {
-                            Button btn_1_0 = btn_grid[x + 1, y];
-                            int i_1_0 = SurroundCount.When0(btn_1_0, btn_grid);
-                            Numbers.DisplayCount(Numbers.MineCount(btn_1_0, btn_grid), btn_1_0);
-                        }
-                        catch (Exception) { }
-                        try
-                        {
-                            Button btn_m1_1 = btn_grid[x - 1, y + 1];
-                            int i_m1_1 = SurroundCount.When0(btn_m1_1, btn_grid);
-                            Numbers.DisplayCount(Numbers.MineCount(btn_m1_1, btn_grid), btn_m1_1);
-                        }
-                        catch (Exception) { }
-                        try
-                        {
-                            Button btn_0_1 = btn_grid[x, y + 1];
-                            int i_0_1 = SurroundCount.When0(btn_0_1, btn_grid);
-                            Numbers.DisplayCount(Numbers.MineCount(btn_0_1, btn_grid), btn_0_1);
-                        }
-                        catch (Exception) { }
-                        try
-                        {
-                            Button btn_1_1 = btn_grid[x + 1, y + 1];
-                            int i_1_1 = SurroundCount.When0(btn_1_1, btn_grid);
-                            Numbers.DisplayCount(Numbers.MineCount(btn_1_1, btn_grid), btn_1_1);
-                        }
-                        catch (Exception) { }
-                    }
-                }
-            }
-        }
+        ///// <summary>
+        ///// expands stuff.
+        ///// </summary>
+        //public void Expansion(Button myButton)//think of better name
+        //{
+        //    for (int x = 0; x < 15; x++)//for the horizontal buttons.
+        //    {
+        //        for (int y = 0; y < 15; y++)//for the vertical buttons.
+        //        {
+        //            if (btn_grid[x, y] == myButton)//gets position of mybtn.
+        //            {
+        //                /*
+        //                  +----+----+----+
+        //                  |x-1 | x  |x+1 | 
+        //                  +----+----+----+ 
+        //                  |x-1 |mybn|x+1 |
+        //                  +----+----+----+  
+        //                  |x-1 | x  |x+1 | 
+        //                  +----+----+----+ 
+        //                */
+        //                //try
+        //                //{
+        //                    Button btn_m1_m1 = btn_grid[x - 1, y - 1];
+        //                    int i_m1_m1 = SurroundCount.When0(btn_m1_m1, btn_grid);
+        //                    Numbers.DisplayCount(Numbers.MineCount(btn_m1_m1, btn_grid), btn_m1_m1);
+        //                //}
+        //                //catch (Exception) { }
+        //                try
+        //                {
+        //                    Button btn_0_m1 = btn_grid[x, y - 1];
+        //                    int i_0_m1 = SurroundCount.When0(btn_0_m1, btn_grid);
+        //                    Numbers.DisplayCount(Numbers.MineCount(btn_0_m1, btn_grid), btn_0_m1);
+        //                }
+        //                catch (Exception) { }
+        //                try
+        //                {
+        //                    Button btn_1_m1 = btn_grid[x + 1, y - 1];
+        //                    int i_1_m1 = SurroundCount.When0(btn_1_m1, btn_grid);
+        //                    Numbers.DisplayCount(Numbers.MineCount(btn_1_m1, btn_grid), btn_1_m1);
+        //                }
+        //                catch (Exception) { }
+        //                try
+        //                {
+        //                    Button btn_m1_0 = btn_grid[x - 1, y];
+        //                    int i_m1_0 = SurroundCount.When0(btn_m1_0, btn_grid);
+        //                    Numbers.DisplayCount(Numbers.MineCount(btn_m1_0, btn_grid), btn_m1_0);
+        //                }
+        //                catch (Exception) { }
+        //                try
+        //                {
+        //                    Button btn_1_0 = btn_grid[x + 1, y];
+        //                    int i_1_0 = SurroundCount.When0(btn_1_0, btn_grid);
+        //                    Numbers.DisplayCount(Numbers.MineCount(btn_1_0, btn_grid), btn_1_0);
+        //                }
+        //                catch (Exception) { }
+        //                try
+        //                {
+        //                    Button btn_m1_1 = btn_grid[x - 1, y + 1];
+        //                    int i_m1_1 = SurroundCount.When0(btn_m1_1, btn_grid);
+        //                    Numbers.DisplayCount(Numbers.MineCount(btn_m1_1, btn_grid), btn_m1_1);
+        //                }
+        //                catch (Exception) { }
+        //                try
+        //                {
+        //                    Button btn_0_1 = btn_grid[x, y + 1];
+        //                    int i_0_1 = SurroundCount.When0(btn_0_1, btn_grid);
+        //                    Numbers.DisplayCount(Numbers.MineCount(btn_0_1, btn_grid), btn_0_1);
+        //                }
+        //                catch (Exception) { }
+        //                try
+        //                {
+        //                    Button btn_1_1 = btn_grid[x + 1, y + 1];
+        //                    int i_1_1 = SurroundCount.When0(btn_1_1, btn_grid);
+        //                    Numbers.DisplayCount(Numbers.MineCount(btn_1_1, btn_grid), btn_1_1);
+        //                }
+        //                catch (Exception) { }
+        //            }
+        //        }
+        //    }
+        //}
     }
 }
